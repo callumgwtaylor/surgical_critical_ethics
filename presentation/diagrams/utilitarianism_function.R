53e8cae585f633824d817d2bea721a47ff20183c
Callum <callumgwtaylor@gmail.com>
2021-06-19 18:55:08 +0100

    updated video file

--- presentation/diagrams/utilitarianism_function.R
+++ presentation/diagrams/utilitarianism_function.R
@@ -41,11 +41,18 @@ utility_plot <- ggplot2::ggplot(data = utility) +
   theme_minimal() +
   theme(panel.grid = element_blank()) +
   theme(plot.title = element_text(family = "Vivaldi",
-                                  size = 50,
-                                  vjust = -25))
+                                  size = 100,
+                                  vjust = -15,
+                                  hjust = 0.5),
+        plot.background = element_rect(colour = "#FDF6E3",
+                                       fill = "#FDF6E3"),
+        panel.background = element_rect(colour = "#FDF6E3",
+                                        fill = "#FDF6E3"))
   
 
 anim_save(animation = utility_plot,
           path = "C:/Users/desktop-coll/Dropbox/Medicine/Academic.9.ICM/surgical_critical_ethics/presentation/diagrams",
           filename = "utility.mp4",
-          renderer = ffmpeg_renderer())
+          renderer = ffmpeg_renderer(),
+          width = 1800,
+          height = 1200)
